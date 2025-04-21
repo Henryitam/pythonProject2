74d848c40d6aaff74f8662be5e5c6a1a542423a7
HenryItam <111151129+Henryitam@users.noreply.github.com>
2025-04-20 22:01:57 -0700

    Update kwargs_tutorial.py

--- kwargs_tutorial.py
+++ kwargs_tutorial.py
@@ -7,3 +7,15 @@ fun(name="bassy",age="12",sex="male")
 # In Python, *args and **kwargs are used to allow functions to accept an
 # arbitrary number of arguments. These features provide great flexibility when designing
 # functions that need to handle a varying number of inputs.
+
+#                ARGS
+# multiplying multiple numbers using the arg functions
+
+def multiply_numbers(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+
+print(multiply_numbers(2,3,4,5))
